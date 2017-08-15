d36c33ace2d6a6e2a7f5497f6b8e150530b37a11
Graeme Laurenson <github@actevate.com>
2017-08-15 09:24:51 +0100

    Update buttons vertical margin

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'css'
+sass_dir = '_sass'
+images_dir = 'images'
+javascripts_dir = 'javascripts'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
